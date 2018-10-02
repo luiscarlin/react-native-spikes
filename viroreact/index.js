d3c80043f792b356d81c783772a7e08164aeeb66
Luis Carlin <luiscarlin@gmail.com>
2018-10-02 07:54:17 -0400

    create project using viro

--- viroreact/index.js
+++ viroreact/index.js
@@ -1,7 +1,7 @@
-/** @format */
+import { AppRegistry } from 'react-native';
+import App from './App.js';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+AppRegistry.registerComponent('viroreact', () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+// The below line is necessary for use with the TestBed App
+AppRegistry.registerComponent('ViroSample', () => App);
